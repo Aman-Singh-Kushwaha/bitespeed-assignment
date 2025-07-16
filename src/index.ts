f0701fd3b281985732ed5f2b74ba894abf5b4a75
Aman Kushwaha <amansinghkushwaha2003@gmail.com>
2025-07-16 20:51:40 +0530

    init: project scaffold

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, Bitespeed!');
+});
