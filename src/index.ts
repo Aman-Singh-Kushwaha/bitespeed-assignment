846c70f60bd7c62359c3c38ea2a4ff6f880a0267
Aman Kushwaha <amansinghkushwaha2003@gmail.com>
2025-07-16 21:26:40 +0530

    init: db model and setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
-const app = express();
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('/health', (req: Request, res: Response) => {
+  res.sendStatus(200);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello, Bitespeed!');
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
